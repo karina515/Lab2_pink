676abc2921d8d974effade8e7de8d627c0600583
Максим Максимович Юшков <19300632@live.inueco.ru>
2021-02-19 09:42:47 +0500

    file1

---
+++ bank.js
@@ -0,0 +1,13 @@
+//
+
+
+function CloseCardLimit() {
+    let limit;
+    limit = 10000
+        for (let i=0; i==limit; i++) {
+            if(i=limit){
+                console.log('карта заблокированна')
+            }
+    }
+    alert(' ') 
+}
