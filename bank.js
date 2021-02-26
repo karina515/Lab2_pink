20932143f6d70ad33ffdcc9f0fc9c54379c18322
Максим Максимович Юшков <19300632@live.inueco.ru>
2021-02-26 11:38:57 +0500

    version 3

--- bank.js
+++ bank.js
@@ -12,24 +12,34 @@ let pay = 20000;
         let percent = 50;
          let Mpay = pay + (pay / 100 * percent)
 
+закрытие лимита 
+x = 40000 лимит исчерпан 
+  когда он исчерпан нужно его закрыть 
+  x=40000
+  i=Mpay= платеж +процент
+  b= закрытие долга= x-i
 
 */
 
-function Ccl() {
-    let closelimit= 0;
-    for(let Debt = 0; Debt>closelimit; Debt++); {
-     if (Debt=40000){   
-     let pay = 20000;
-        let percent = 50;
-         let Mpay = pay + (pay / 100 * percent)  
-         let closeDebt = Debt-Mpay
+function Ccl(limit, percent, change) {
+    let mp;//вычислить ежемесячный процент
+    let dept = limit;
+    let time = 1;
+    while( dept > 0) {
+        if (Debt==40000); {   
+            let pay = 20000;
+            let percent = 50;
+            let Mpay = pay + (pay / 100 * percent)  
+            let closelimit = Debt-Mpay
          }
-         if(closelimit=closeDebt){
+         if(closelimit==Debt){
              console.log('лимит закрыт ')
          }else{
              console.log('лимит не закрыт')
          }
+
+         time++;
      }
  //alert (Ввести + '  ')
  }
-Ccl();
+Ccl(40000, 24, 0);
