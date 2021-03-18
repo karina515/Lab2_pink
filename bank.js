b7f1e31e6ed231f1c5269ddd448785073cd4ce38
Андрей Алексеевич Платонов <19300586@live.inueco.ru>
2021-03-18 10:47:35 +0500

    Beautiful

--- bank.js
+++ bank.js
@@ -30,25 +30,38 @@ function Ccl(limit, percent, change) {
      //вычислить ежемесячный процент
     let debt = limit;
     let time = 1;
+    let ch = change;
     while( debt > 0) {
-
+  
         let mp= debt*(percent/(100*12));
         
         console.log('начисленные проценты' , mp.toFixed() )
         let mr = mp*2;
-        if(mr < 500) {
-        mr=500}
+        if(mr < 500){
+            mr=500
+        }
         
         console.log('пополнение' ,mr.toFixed())
-         debt = debt - mr;
-         if(debt==0){
+        debt = debt - mr;
+        if(debt < 0){
              console.log('лимит закрыт: ', debt.toFixed())
-         }else{
+        }
+        else{
              console.log('лимит не закрыт: ', debt.toFixed())
-            } 
+        } 
 
-         time++;
-     }
+        if(ch > 0){
+            debt -= ch;
+        }
+        else if (ch < 0){
+            debt += ch
+        }
+        else if(ch==0){
+            console.log("Так не работает")
+        }
+
+        time++;
+    }
  //alert (Ввести + '  ')
  }
 Ccl(40000, 24, 0);
