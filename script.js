f1fb5c57d33ef357a695b4c312ddcb4722ff2af1
Андрей Алексеевич Платонов <19300586@live.inueco.ru>
2021-04-08 09:46:32 +0500

    Yan

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert ('шо ты тут забыл')
+
+
+// document.getElementById()
+// document.querySelector()
+// document.querySelectorAll()
+let myInput = document.getElementById("input_myvalue"),
+myButton =  document.getElementById("my_button");
+
+myButton.addEventListener("click", 
+    (event) => {
+        let value = myInput.value;
+        alert ('Значение в импуте - ' + value);
+    }
+);
+setTimeout( () => {
+    document.getElementById("textblock").innerHTML = `
+        Привет! <br>
+        <b>Этот html вставлен в виде строки из скрипта</b>. <br>
+        Здорово, правда?
+    `;
+}, 3000);
